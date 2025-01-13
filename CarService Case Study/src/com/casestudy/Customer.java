af1b35e413677c354a785ff0efbebb55025568de
Arceus-1817 <163709917+Arceus-1817@users.noreply.github.com>
2025-01-13 12:06:51 +0530

    Case Study start

---
+++ CarService Case Study/src/com/casestudy/Customer.java
@@ -0,0 +1,10 @@
+package com.casestudy;
+
+public class Customer {
+	protected String name;
+	protected double ph_no;
+	protected double car_no;
+	protected String car_name;
+	protected String C_address;
+	
+}
