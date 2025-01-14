debc33d867156dd448130a0aef0c897f47db5f1e
Kunal <kunal@gmail.com>
2025-01-14 12:20:38 +0530

    partsSubMenu

--- CarService Case Study/src/com/casestudy/Parts.java
+++ CarService Case Study/src/com/casestudy/Parts.java
@@ -1,19 +1,20 @@
 package com.casestudy;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Parts implements Serializable {
 
 	private String PartId;
 	private String PartName;
-	private double prize;
+	private double Prize;
 	
 	
 	
-	public Parts(String partId, String partName, double prize) {
+	public  Parts(String partId, String partName, double prize) {
 		PartId = partId;
 		PartName = partName;
-		this.prize = prize;
+		this.Prize = prize;
 	}
 	public String getPartId() {
 		return PartId;
@@ -28,14 +29,29 @@ public class Parts implements Serializable {
 		PartName = partName;
 	}
 	public double getPrize() {
-		return prize;
+		return Prize;
 	}
-	public void setPrize(double prize) {
-		this.prize = prize;
+	public  void setPrize(double prize) {
+		 Prize= prize;
 	}
 	@Override
 	public String toString() {
-		return "Parts [PartId=" + PartId + ", PartName=" + PartName + ", prize=" + prize + "]";
+		return "Parts [PartId=" + PartId + ", PartName=" + PartName + ", prize=" + Prize + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(PartId);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Parts other = (Parts) obj;
+		return Objects.equals(PartId, other.PartId);
 	}
 	
 	
