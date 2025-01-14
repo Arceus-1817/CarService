8b27f44c2b4e7356a1a1a6340d31ce029d94f221
Kunal <kunal@gmail.com>
2025-01-14 09:33:12 +0530

    services

---
+++ CarService Case Study/src/com/casestudy/ServiceStation.java
@@ -0,0 +1,19 @@
+package com.casestudy;
+import java.util.*;
+
+public class ServiceStation {
+
+	private List <Customer> customers;
+	private List <Parts> parts;
+	private List <Bill> bills;
+	private List <Vehicle> vehicles;
+	
+	public ServiceStation() {
+	 customers=new hashcode<>();
+	 
+	
+	
+	
+	}
+	
+}
